24b0b8d23ec44c34fefacf53e4d968be326b6de7
JIMMYALBERTO <119382123+JIMMYALBERTO@users.noreply.github.com>
2023-09-13 14:44:36 -0600

    Add files via upload

---
+++ U20230292_Semana2.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //DECLARACION DE VARIABLES
+
+        //ITEMS 1
+
+        int edad= 30;
+
+        string objeto="computadora";
+   
+        //ITEMS 2
+
+        float salaria=502.25;
+
+        decimal precio = 20.3658;
+
+        //ITEMS 3
+
+        bool activo=true;
+
+        //AUTOEVALUCION=10.
+            
+
+
+
+
+        }
+        
+
+
+
+
+    }
+}
